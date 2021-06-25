47fb325b7dab5f699e41c28ee74c3c92df19eba0
PelumiWeb <pelumiogundipe905@gmail.com>
2021-06-25 21:49:55 +0100

    deployment bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      }
+    },
     colors: {
       backgroundColor: '#062021',
       white: '#fff' 
